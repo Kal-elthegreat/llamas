268457eda06a23bd5e29f5668953628a538c53e5
mpackianath <mani_packianathan@intuit.com>
2022-02-10 17:46:59 -0500

    fixing voter state

--- final-proj/src/components/register/VoterTool.js
+++ final-proj/src/components/register/VoterTool.js
@@ -7,13 +7,15 @@ export const VoterTool = () =>{
     const llamasStore = useLlamasStore();
     return(
         <>
-        <VoterForm buttonText="Register Voter"></VoterForm>
+        <VoterForm buttonText="Register Voter" onSubmitVoterForm={llamasStore.addVoter}></VoterForm>
 
  
         <VoterTable voters={llamasStore.voters} 
-                onEditVoterID={llamasStore.editVoterId}
+                editVoterID={llamasStore.editVoterId}
+                onEditVoter={llamasStore.editVoter}
                 onDeleteVoter={llamasStore.deleteVoter} 
                 onClickSave={llamasStore.saveVoter} 
+                sortObj={llamasStore.sortObj}
                 // sortButtonText={llamasStore.votersSort.sortCol + "-" + voterToolStore.votersSort.sortDir} 
                 />
                 </>
