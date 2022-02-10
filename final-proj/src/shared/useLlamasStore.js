dc1b3c1b6f29bb078ea5198af53264add7b15924
mpackianath <mani_packianathan@intuit.com>
2022-02-10 17:13:20 -0500

    added election form

--- final-proj/src/shared/useLlamasStore.js
+++ final-proj/src/shared/useLlamasStore.js
@@ -1,22 +1,26 @@
 import { useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
-import { createViewResultsAction,createShouldDisplayResultsAction } from "../components/election/actions/electionActions";
+import { createViewResultsAction,createShouldDisplayResultsAction, createAddElectionRequestAction,refreshElections } from "../components/election/actions/electionActions";
 import { addVoter, refreshVoters,saveVoter,deleteVoter, createSortVotersAction, createEditVoterAction, createCancelEditVoterAction } from "../components/register/registerVoterActions";
 import { selectEditVoterId, selectSortedVoters, selectSortObj } from "../components/register/registerVoterSelectors";
 
 
 export const useLlamasStore = ()=> {
+    //voters
     const voters = useSelector(selectSortedVoters);
     const editVoterId = useSelector(selectEditVoterId);
     const sortObj = useSelector(selectSortObj);
+
+    //elections
+    const elections = useSelector(state=> state.elections);
     const election = useSelector(state => state.election);
     const shouldDisplay = useSelector(state => state.shouldDisplay);
-    console.log(election,shouldDisplay)
 
     const dispatch = useDispatch();
 
     const actions = useMemo(()=> bindActionCreators({
+        //Voters
         refreshVoters,
         addVoter,
         saveVoter,
@@ -24,13 +28,20 @@ export const useLlamasStore = ()=> {
         sortVoters:createSortVotersAction,
         editVoter: createEditVoterAction,
         cancelEditVoter:createCancelEditVoterAction,
+
+        //Elections
         viewResults:createViewResultsAction,
         shouldDisplayResults:createShouldDisplayResultsAction,
+        addElection: createAddElectionRequestAction,
+        refreshElections,
     },dispatch),[dispatch]);
 
     useEffect(()=>{
         actions.refreshVoters();
+        actions.refreshElections();
     },[actions]);
 
-    return {...actions, voters, editVoterId, sortObj,election,shouldDisplay};
+    return {...actions, 
+        voters, editVoterId, sortObj,
+        election,shouldDisplay,elections};
 }
