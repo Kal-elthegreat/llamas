f881ab7767da0ff21145090195493b3bca6e1e56
Kiel Lewis <kiel.lewis@mailchimp.com>
2022-02-10 11:49:07 -0500

    added dependencies

--- final-proj/src/components/election/createElection.js
+++ final-proj/src/components/election/createElection.js
@@ -1,5 +1,60 @@
+import {ElectionsTable} from './ElectionsTable'
+import {ElectionResults} from './ElectionResults'
+
 export const CreateElection = () => {
+    // grab necessary state from store here and pass to children
     return (
-        <h1>create election here</h1>
+        <div>
+            {/* <Form /> */}
+            <ElectionsTable data={data}/>
+            <ElectionResults shouldDisplay={false}/>
+        </div>
     )
 }
+
+const data = [
+    {
+        "id": 2,
+        "name": 'test',
+        "questionnaire": [
+            {
+                "id": 1,
+                "question": "Pets need to be considered as dependents for tax purposes?",
+                "yesCount": 0,
+                "noCount": 4,
+                "voterIds": [1,17,92,35]
+            },
+            {
+                "id": 2,
+                "question": "Public transport should be free?",
+                "yesCount": 4,
+                "noCount": 0,
+                "voterIds": [1,17,92,35]
+            },
+            {
+                "id": 3,
+                "question": "Pets need to be considered as dependents for tax purposes?",
+                "yesCount": 2,
+                "noCount": 2,
+                "voterIds": [1,17,92,35]
+            },
+        ]
+    },
+    {
+        "id": 3,
+        "name": 'another one',
+        "questionnaire": [
+            {
+                "id": 1,
+                "question": "Pets need to be considered as dependents for tax purposes?",
+                "pollCount": 0
+            },
+            {
+                "id": 2,
+                "question": "Public transport should be free?",
+                "pollCount": 0
+            }
+        ]
+    },
+
+]
